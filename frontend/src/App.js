d887b4e00375cd1da5525498ffefdfd0cd1ad040
Jose Jimenez <joseluisjimagui@gmail.com>
2023-09-08 02:01:42 -0600

    Add react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import Header from './components/Header'
+import Login from './components/Login'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />}/>                           
+        
+      </Routes>
+    </BrowserRouter>
+
     </div>
   );
 }
